73f86edea0e5ce804b324deba3bd8ea9c9f3c539
afiqket <muhdafiqaiman123@gmail.com>
2025-06-05 17:11:50 +0900

    removed comments

--- test2.c
+++ test2.c
@@ -13,16 +13,14 @@ main(int argc, char *argv[])
     int tid, jtid, waitret, childpid;
     printf(1, "test2...\n");
 
-    // ---- Allocate PGSIZE + PGSIZE bytes so we can align a 4 KB page for the thread stack ---- 
     buf = malloc(PGSIZE + PGSIZE);
     if (buf == 0) {
         printf(1, "ERROR: malloc failed\n");
         exit();
     }
-    // Round up to the next 4 KB boundary
+
     stack = (void*)(((uint)buf + PGSIZE) & ~(PGSIZE - 1));
 
-    // ---- Create a thread ---- 
     tid = clone(stack);
     if (tid < 0) {
         printf(1, "ERROR: clone failed\n");
@@ -33,10 +31,9 @@ main(int argc, char *argv[])
         // In the child thread
         sleep(20);
         printf(1, "4. Child thread: Finish second\n");
-        exit();  // terminate this thread
+        exit();
     }
 
-    // ---- Create a child process ---- 
     childpid = fork();
     if (childpid < 0) {
         printf(1, "ERROR: fork failed\n");
@@ -47,14 +44,14 @@ main(int argc, char *argv[])
         // In the child thread
         sleep(10);
         printf(1, "3. Child process: Finish first\n");
-        exit();  // terminate this thread
+        exit();
     }
 
     printf(1, "1. Parent: created a thread and child process\n");
     
     printf(1, "2. Parent: wait for thread first\n");
+
     jtid = join();
-    // printf(1, "Parent: jtid = %d\n", jtid);
     if (jtid < 0) {
         printf(1, "ERROR: join failed\n");
         exit();
@@ -63,7 +60,6 @@ main(int argc, char *argv[])
     }
 
     waitret = wait();
-    // printf(1, "Parent: waitret = %d\n", waitret);
     if (waitret < 0) {
         printf(1, "ERROR: wait failed\n");
         exit();
@@ -73,7 +69,6 @@ main(int argc, char *argv[])
 
     printf(1, "7. Test done. Prints should be in order 1-7\n");
 
-    // 5) Free the buffer used for the thread’s stack
     free(buf);
     exit();
 }
