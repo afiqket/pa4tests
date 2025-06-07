078c1bae6743f1e9d89971f5f6dda2ff79eb7d89
afiqket <muhdafiqaiman123@gmail.com>
2025-06-07 20:11:21 +0900

    edites testclone and testerror

--- testclone.c
+++ testclone.c
@@ -11,12 +11,12 @@ main(int argc, char *argv[])
     void *stack1, *stack2;
     int tid1, tid2, jtid1, jtid2;
 
-    printf(1, "[clonetest4: Child thread can call clone, but they share the same thread parent]\n");
+    printf(1, "1. testclone...\n");
 
     // 1) Allocate PGSIZE + PGSIZE bytes so we can align a 4 KB page for the thread stack
     buf = malloc(PGSIZE + PGSIZE + PGSIZE);
     if (buf == 0) {
-        printf(1, "clonetest4: malloc failed\n");
+        printf(1, "ERROR: malloc failed\n");
         exit();
     }
     // Round up to the next 4 KB boundary
@@ -28,7 +28,7 @@ main(int argc, char *argv[])
     // 2) Create a single thread
     tid1 = clone(stack1);
     if (tid1 < 0) {
-        printf(1, "Parent: clone failed\n");
+        printf(1, "ERROR: Parent: clone failed\n");
         free(buf);
         exit();
     }
@@ -38,41 +38,41 @@ main(int argc, char *argv[])
         // printf(1, "Thread B or C: tid = %d\n", tid2);
         if (tid2 < 0) {
             sleep(50);
-            printf(1, "Thread B: clone failed\n");
+            printf(1, "ERROR: Thread B: clone failed\n");
             free(buf);
             exit();
         }
         if (tid2 == 0) {
             // In the child thread C
-            sleep(100);
-            printf(1, "Thread C: Hello world!\n");
+            sleep(20);
+            printf(1, "5. Thread C: Hello world!\n");
             exit(); 
         }
-        sleep(50);
-        printf(1, "Thread B: Created thread C (tid = %d, pid = %d)\n", tid2, getpid());
+        sleep(10);
+        printf(1, "3. Thread B: Created thread C (tid = %d, pid = %d)\n", tid2, getpid());
         exit(); 
     }
 
     // 3) Back in the parent: clone() returned child’s TID
     sleep(10);
-    printf(1, "Parent: Created thread B (tid = %d, pid = %d)\n", tid1, getpid());
+    printf(1, "2. Parent: Created thread B (tid = %d, pid = %d)\n", tid1, getpid());
 
     // 4) Wait for the child thread to exit
     jtid1 = join();
     if (jtid1 < 0) {
-        printf(1, "clonetest4: join failed\n");
+        printf(1, "ERROR: first join failed\n");
     } else {
-        printf(1, "Parent: successfully joined thread B (tid = %d)\n", jtid1);
+        printf(1, "4. Parent: successfully joined thread B (tid = %d)\n", jtid1);
     }
 
     jtid2 = join();
     if (jtid2 < 0) {
-        printf(1, "clonetest4: join failed\n");
+        printf(1, "ERROR: second join failed\n");
     } else {
-        printf(1, "Parent: successfully joined thread C (tid = %d)\n", jtid2);
+        printf(1, "6. Parent: successfully joined thread C (tid = %d)\n", jtid2);
     }
 
-    printf(1, "Test done. There should not be any zombies or crashes\n", jtid2);
+    printf(1, "7. Test done. There should not be any zombies or crashes\n", jtid2);
 
 
     // 5) Free the buffer used for the thread’s stack
--- testerror.c
+++ testerror.c
@@ -14,21 +14,21 @@ main(int argc, char *argv[])
     // printf(1, "addr of padding[0]    = 0x%x\n", &padding[0]);
     // printf(1, "addr of padding[950] = 0x%x\n", &padding[950]);
 
-    printf(1, "[clonetest6: Test simple error cases]\n");
+    printf(1, "testerror...\n");
 
     jtid = join();
     printf(1, "1. Join with no threads (should fail and return -1)\n");
     if (jtid == -1){
-        printf(1, "CORRECT: join() = %d\n", jtid);        
+        printf(1, "\tCORRECT: join() = %d\n", jtid);        
     } else {
-        printf(1, "WRONG: join() = %d\n", jtid);  
+        printf(1, "\tWRONG: join() = %d\n", jtid);  
     }
 
 
     // 1) Allocate PGSIZE + PGSIZE bytes so we can align a 4 KB page for the thread stack
     buf = malloc(PGSIZE + PGSIZE);
     if (buf == 0) {
-        printf(1, "clonetest6: malloc failed\n");
+        printf(1, "ERROR: malloc failed\n");
         exit();
     }
     // Round up to the next 4 KB boundary
@@ -40,18 +40,18 @@ main(int argc, char *argv[])
     printf(1, "2. Clone with stack that is not page-aligned (should fail) (stack = 0x%x)\n", unaligned_stack);
     tid = clone(unaligned_stack);
     if (tid == -1) {
-        printf(1, "CORRECT: clone() failed and returned %d\n", tid);
+        printf(1, "\tCORRECT: clone() failed and returned %d\n", tid);
     }
     else {
         // In the child thread
         sleep(10);
-        printf(1, "WRONG: clone() succeeded even with unaligned stack\n");
+        printf(1, "\tWRONG: clone() succeeded even with unaligned stack\n");
         exit();  // terminate this thread
     }
 
     tid = clone(stack);
     if (tid < 0) {
-        printf(1, "clonetest6: clone failed\n");
+        printf(1, "\tERROR: clone failed\n");
         free(buf);
         exit();
     }
@@ -64,17 +64,17 @@ main(int argc, char *argv[])
         }
         printf(1, "3. Thread child invokes fork() (should fail and return -1)\n");
         if (forkret == -1){
-            printf(1, "CORRECT: forkret() = %d\n", forkret);        
+            printf(1, "\tCORRECT: forkret() = %d\n", forkret);        
         } else {
-            printf(1, "WRONG: forkret() = %d\n", forkret);  
+            printf(1, "\tWRONG: forkret() = %d\n", forkret);  
         }
 
         jtid = join();
         printf(1, "4. Thread child invokes join() (should fail and return -1)\n");
         if (jtid == -1){
-            printf(1, "CORRECT: join() = %d\n", jtid);        
+            printf(1, "\tCORRECT: join() = %d\n", jtid);        
         } else {
-            printf(1, "WRONG: join() = %d\n", jtid);  
+            printf(1, "\tWRONG: join() = %d\n", jtid);  
         }
 
         
@@ -87,7 +87,7 @@ main(int argc, char *argv[])
     // 4) Wait for the child thread to exit
     jtid = join();
     if (jtid < 0) {
-        printf(1, "clonetest6: join failed\n");
+        printf(1, "ERROR: join failed\n");
     } 
     // else {
     //     printf(1, "(Parent: joined thread)\n");
@@ -96,11 +96,12 @@ main(int argc, char *argv[])
     jtid = join();
     printf(1, "5. All threads should be joined by now. No threads left. Join should fail\n");
     if (jtid == -1){
-        printf(1, "CORRECT: join() = %d\n", jtid);        
+        printf(1, "\tCORRECT: join() = %d\n", jtid);        
     } else {
-        printf(1, "WRONG: join() = %d\n", jtid);  
+        printf(1, "\tWRONG: join() = %d\n", jtid);  
     }
 
+    printf(1, "6. Test done. All tests from 1-5 must print CORRECT.\n");
 
     // 5) Free the buffer used for the thread’s stack
     free(buf);
